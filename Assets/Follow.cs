8c238f6cf301a4797f1be88607df512d4527c836
Aaron Nyaanga <112010276+Aaron23713@users.noreply.github.com>
2023-10-31 17:32:41 -0400

    Build 1.1
    Semi Functionality for Grab

--- Assets/Follow.cs
+++ Assets/Follow.cs
@@ -8,9 +8,14 @@ public class Follow : MonoBehaviour
     public Transform otherTarget;
     const float EPSILON = 0.1f;
      
+     public Health myHealth;
     public float speed;
-    bool updateWorthy = false;
+    // bool updateWorthy = false;
+
+    private Vector3 velocity = Vector3.zero;
     // Start is called before the first frame update
+    public AudioSource heartBeat;
+    public AudioSource flatline;
     void Start()
     {
         
@@ -19,15 +24,32 @@ public class Follow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(updateWorthy){
+        // if(updateWorthy){
             otherTarget.transform.LookAt(theTarget.position);
            
-            // if((otherTarget.transform.position - theTarget.position).magnitude > EPSILON){
+            if((otherTarget.transform.position - theTarget.position).magnitude > EPSILON){
                 otherTarget.transform.Translate(0.0f,0.0f, speed * Time.deltaTime);
-            // }
-        }
+            }
+        // }
     }
     public void newMeth(){
-        updateWorthy = true;
+        // updateWorthy = true;
+    }
+
+    public void OnTriggerEnter(Collider col){
+        if(col.gameObject.tag == "Player"){
+            Debug.Log("HIT FOUND");
+            if(myHealth.playerHealth >= 10){
+                flatline.Play();
+            }else{
+            myHealth.playerHealth++;
+            heartBeat.Play();
+            // StartCoroutine(Timedelay());
+           
+            }
+        }
+    }
+    IEnumerator Timedelay(){
+        yield return new WaitForSeconds(10f);
     }
 }
